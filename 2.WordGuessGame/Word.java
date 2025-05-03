1dd7b9dd4568dcc7183ffcc67b763dcc5ef2632c
sherviiinn <shervinshaebani1385@gmail.com>
2025-05-03 21:57:31 +0330

    Word class added

---
+++ 2.WordGuessGame/Word.java
@@ -0,0 +1,12 @@
+public class Word {
+    private String word;
+    public Word(String word) {
+        this.word = word;
+    }
+    public String getWord() {
+        return word;
+    }
+    public int wordlength(){
+        return word.length();
+    }
+}
